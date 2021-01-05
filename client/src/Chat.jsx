129a1cb384c5b6dee5eb60cfba270a08900eee8c
Riya <riyanegi221b@gmail.com>
2021-01-05 20:51:19 +0530

    add client subscription

--- client/src/Chat.jsx
+++ client/src/Chat.jsx
@@ -1,83 +1,109 @@
-import React from "react"
-import { ApolloClient, InMemoryCache, ApolloProvider, useQuery, gql, useMutation } from '@apollo/client';
+import React from "react";
+import {
+    ApolloClient,
+    InMemoryCache,
+    ApolloProvider,
+    useSubscription,
+    useMutation,
+    gql,
+} from "@apollo/client";
+import { WebSocketLink } from "@apollo/client/link/ws";
 import { Container, Row, Col, FormInput, Button } from "shards-react";
 
+const link = new WebSocketLink({
+    uri: `ws://localhost:4000/`,
+    options: {
+        reconnect: true,
+    },
+});
 
 const client = new ApolloClient({
-    uri: 'http://localhost:4000/',
-    cache: new InMemoryCache()
+    link,
+    uri: "http://localhost:4000/",
+    cache: new InMemoryCache(),
 });
 
 const GET_MESSAGES = gql`
-query {
-    messages{
+  subscription {
+    messages {
       id
-      user
       content
+      user
     }
-  }`;
+  }
+`;
 
-const POST_MESSAGE = gql`mutation($user:String!, $content:String!){
-    postMessage(user:$user, content:$content)
-  }` ;
-
+const POST_MESSAGE = gql`
+  mutation($user: String!, $content: String!) {
+    postMessage(user: $user, content: $content)
+  }
+`;
 
 const Messages = ({ user }) => {
-    const { data } = useQuery(GET_MESSAGES);
+    const { data } = useSubscription(GET_MESSAGES);
     if (!data) {
         return null;
     }
-    return <div>
-        {data.messages.map(({ id, user: messageUser, content }) => (
-            <div style={{
-                display: 'flex', justifyContent: user === messageUser ? 'flex-end' : 'flex-start',
-                paddingBottom: "1em"
-            }}>
-                {user !== messageUser && (
-                    <div style={{
-                        height: 50,
-                        width: 50,
-                        marginRight: "0.5em",
-                        border: "2px solid #e5e6ea",
-                        borderRadius: 25,
-                        textAlign: "center",
-                        fontSize: "18pt",
-                        paddingTop: 5,
-                    }}> {messageUser.slice(0, 2).toUpperCase()} </div>
-                )}
-                <div style={{
-                    background: user === messageUser ? "#58bf56" : "#e5e6ea",
-                    color: user === messageUser ? "white" : "black",
-                    padding: "1em",
-                    borderRadius: "1em",
-                    maxWidth: "60%"
 
-                }}>
-                    {content}
+    return (
+        <>
+            {data.messages.map(({ id, user: messageUser, content }) => (
+                <div
+                    style={{
+                        display: "flex",
+                        justifyContent: user === messageUser ? "flex-end" : "flex-start",
+                        paddingBottom: "1em",
+                    }}
+                >
+                    {user !== messageUser && (
+                        <div
+                            style={{
+                                height: 50,
+                                width: 50,
+                                marginRight: "0.5em",
+                                border: "2px solid #e5e6ea",
+                                borderRadius: 25,
+                                textAlign: "center",
+                                fontSize: "18pt",
+                                paddingTop: 5,
+                            }}
+                        >
+                            {messageUser.slice(0, 2).toUpperCase()}
+                        </div>
+                    )}
+                    <div
+                        style={{
+                            background: user === messageUser ? "#17c671" : "#e5e6ea",
+                            color: user === messageUser ? "white" : "black",
+                            padding: "1em",
+                            borderRadius: "1em",
+                            maxWidth: "60%",
+                        }}
+                    >
+                        {content}
+                    </div>
                 </div>
-            </div>
-        ))}
-    </div>
-}
-
+            ))}
+        </>
+    );
+};
 const Chat = () => {
     const [state, stateSet] = React.useState({
         user: "Jack",
         content: "",
     });
-
     const [postMessage] = useMutation(POST_MESSAGE);
-
     const onSend = () => {
         if (state.content.length > 0) {
-            postMessage({ variables: state, })
+            postMessage({
+                variables: state,
+            });
         }
         stateSet({
             ...state,
-            content: ''
-        })
-    }
-
+            content: "",
+        });
+    };
     return (
         <Container>
             <Messages user={state.user} />
@@ -118,9 +144,10 @@ const Chat = () => {
                 </Col>
             </Row>
         </Container>
-    )
-}
-
+    );
+};
 export default () => (
-    <ApolloProvider client={client}><Chat /></ApolloProvider>
-)
+    <ApolloProvider client={client}>
+        <Chat />
+    </ApolloProvider>
+);
